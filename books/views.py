8d88e2aabdf670e7c0f308978d550d5d9e1a7d31
potherpothik <145859550+potherpothik@users.noreply.github.com>
2025-03-03 00:29:27 +0600

    Update views.py

--- books/views.py
+++ books/views.py
@@ -6,22 +6,22 @@ from .forms import BookForm
 
 class BookListView(ListView):
     model = Book
-    template_name = 'index.html'  # Updated template path
+    template_name = 'index.html'
     context_object_name = 'books'
 
 class BookCreateView(CreateView):
     model = Book
     form_class = BookForm
     template_name = 'books/book_form.html'
-    success_url = reverse_lazy('index')  # Changed from 'book_list'
+    success_url = reverse_lazy('index')
 
 class BookUpdateView(UpdateView):
     model = Book
     form_class = BookForm
     template_name = 'books/book_form.html'
-    success_url = reverse_lazy('index')  # Changed from 'book_list'
+    success_url = reverse_lazy('index')
 
 class BookDeleteView(DeleteView):
     model = Book
     template_name = 'books/book_confirm_delete.html'
-    success_url = reverse_lazy('index')  # Changed from 'book_list'
+    success_url = reverse_lazy('index')
