7c6bc8fabb5e4ee1ac0bcf90a7d75e3a89ce77bb
potherpothik <145859550+potherpothik@users.noreply.github.com>
2025-03-03 00:28:38 +0600

    Update urls.py

--- books/urls.py
+++ books/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.BookListView.as_view(), name='index'),  # Changed from 'book_list'
+    path('', views.BookListView.as_view(), name='index'),
     path('create/', views.BookCreateView.as_view(), name='book_create'),
     path('update/<int:pk>/', views.BookUpdateView.as_view(), name='book_update'),
     path('delete/<int:pk>/', views.BookDeleteView.as_view(), name='book_delete'),
